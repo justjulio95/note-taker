a2debc848408f0f0040c591eda3e1b01f973e61b
Julio.S <just.julio95@gmail.com>
2022-05-19 18:02:36 -0400

    minor fixes to ensure the server works as expected.

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes')
 
-//instruct the server on what paths to take using the routes we created
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes)
+//this will grant access to things listed in the public folder - CSS, HTML, JS, etc.
+//NOTE: index.js is initiated with this line. 
+app.use(express.static('public'))
 
 //parse incoming string/array data
 app.use(express.urlencoded({extended: true}));
@@ -14,10 +14,9 @@ app.use(express.urlencoded({extended: true}));
 //parse incoming JSON data
 app.use(express.json())
 
-//this will grant access to things listed in the public folder - CSS, HTML, JS, etc.
-//NOTE: This won't have much, if any effect on this project. It's all BOOTSTRAP.
-//NOTE+: index.js is initiated with this line. 
-app.use(express.static('public'))
+//instruct the server on what paths to take using the routes we created
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now LIVE on http://localhost:${PORT}`)
